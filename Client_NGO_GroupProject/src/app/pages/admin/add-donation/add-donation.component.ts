4d18f47ead5132c89741b30edf6aaf8260848f7c
GoobIsGabe <gabe.harp1996@gmail.com>
2023-11-10 16:13:58 -0500

    added basic pages with descriptors within html

---
+++ Client_NGO_GroupProject/src/app/pages/admin/add-donation/add-donation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-donation',
+  templateUrl: './add-donation.component.html',
+  styleUrls: ['./add-donation.component.css']
+})
+export class AddDonationComponent {
+
+}
---
+++ Client_NGO_GroupProject/src/app/pages/admin/view-donations/view-donations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-donations',
+  templateUrl: './view-donations.component.html',
+  styleUrls: ['./view-donations.component.css']
+})
+export class ViewDonationsComponent {
+
+}
