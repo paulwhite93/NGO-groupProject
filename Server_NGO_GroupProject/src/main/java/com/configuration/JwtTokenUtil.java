20af9a2ebcaa05b1e7bdc61f3e3794e573851929
YYL1222 <99230362+YYL1222@users.noreply.github.com>
2023-11-14 11:15:45 -0600

    Add files via upload

--- Server_NGO_GroupProject/src/main/java/com/configuration/JwtTokenUtil.java
+++ Server_NGO_GroupProject/src/main/java/com/configuration/JwtTokenUtil.java
@@ -8,15 +8,10 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.io.Serializable;
 import java.security.Key;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,16 +27,7 @@ public class JwtTokenUtil{
 
 
     public String extractUsername(String token) {
-    	Claims claims;
-        try {
-            claims = Jwts.parser()
-                    .setSigningKey(SECRET_KEY)
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (Exception e) {
-        	logger.error("Could not get all claims Token from passed token");
-            claims = null;
-        }
+    	Claims claims=extractAllClaims(token);
         String userEmail = claims.getSubject();
         return userEmail;
     }
@@ -78,16 +64,7 @@ public class JwtTokenUtil{
     }
 
     private Date extractExpiration(String token) {
-    	Claims claims;
-        try {
-            claims = Jwts.parser()
-                    .setSigningKey(SECRET_KEY)
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (Exception e) {
-        	logger.error("Could not get all claims Token from passed token");
-            claims = null;
-        }
+    	Claims claims=extractAllClaims(token);
         Date expiration = claims.getExpiration();
         return expiration;
     }
@@ -114,91 +91,3 @@ public class JwtTokenUtil{
     }
 
 }
-//
-//import com.entity.Users;
-//import com.example.springjwt.model.User;
-//import io.jsonwebtoken.*;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Date;
-//import java.util.List;
-//import java.util.concurrent.TimeUnit;
-
-//@Component
-//public class JwtTokenUtil {
-//
-//
-//    private final String secret_key = "mysecretkey";
-//    private long accessTokenValidity = 60*60*1000;
-//
-//    private final JwtParser jwtParser;
-//
-//    private final String TOKEN_HEADER = "Authorization";
-//    private final String TOKEN_PREFIX = "Bearer ";
-//
-//    public JwtTokenUtil(){
-//        this.jwtParser = Jwts.parser().setSigningKey(secret_key);
-//    }
-//
-//    public String createToken(Users user) {
-//        Claims claims = Jwts.claims().setSubject(user.getEmail());
-//        claims.put("firstName",user.getFirstName());
-//        claims.put("lastName",user.getLastName());
-//        Date tokenCreateTime = new Date();
-//        Date tokenValidity = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(accessTokenValidity));
-//        return Jwts.builder()
-//                .setClaims(claims)
-//                .setExpiration(tokenValidity)
-//                .signWith(SignatureAlgorithm.HS256, secret_key)
-//                .compact();
-//    }
-//
-//    private Claims parseJwtClaims(String token) {
-//        return jwtParser.parseClaimsJws(token).getBody();
-//    }
-//
-//    public Claims resolveClaims(HttpServletRequest req) {
-//        try {
-//            String token = resolveToken(req);
-//            if (token != null) {
-//                return parseJwtClaims(token);
-//            }
-//            return null;
-//        } catch (ExpiredJwtException ex) {
-//            req.setAttribute("expired", ex.getMessage());
-//            throw ex;
-//        } catch (Exception ex) {
-//            req.setAttribute("invalid", ex.getMessage());
-//            throw ex;
-//        }
-//    }
-//
-//    public String resolveToken(HttpServletRequest request) {
-//
-//        String bearerToken = request.getHeader(TOKEN_HEADER);
-//        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
-//            return bearerToken.substring(TOKEN_PREFIX.length());
-//        }
-//        return null;
-//    }
-//
-//    public boolean validateClaims(Claims claims) throws AuthenticationException {
-//        try {
-//            return claims.getExpiration().after(new Date());
-//        } catch (Exception e) {
-//            throw e;
-//        }
-//    }
-//
-//    public String extractEmail(Claims claims) {
-//        return claims.getSubject();
-//    }
-//
-//    private List<String> getRoles(Claims claims) {
-//        return (List<String>) claims.get("roles");
-//    }
-//
-//
-//}
