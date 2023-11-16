d73e6befed4c243e5173db3ce4b9e9bf1f885e8d
YYL1222 <99230362+YYL1222@users.noreply.github.com>
2023-11-16 13:29:51 -0600

    Update DonationController.java

--- Server_NGO_GroupProject/src/main/java/com/controller/DonationController.java
+++ Server_NGO_GroupProject/src/main/java/com/controller/DonationController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.entity.Donation_Types;
 import com.entity.Donations;
 import com.entity.Donors;
 import com.service.DonationService;
 import com.service.DonorService;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/dono/")
 @CrossOrigin(origins = "http://localhost:4200")
