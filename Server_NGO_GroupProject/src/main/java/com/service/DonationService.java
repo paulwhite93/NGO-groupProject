3592a8c7369d5f17092ad12ed74efa3c2b8b4966
YYL1222 <99230362+YYL1222@users.noreply.github.com>
2023-11-17 00:23:48 -0600

    Update DonationService.java
    Get a list of donation objects. Save donor first and set all donation same donor.

--- Server_NGO_GroupProject/src/main/java/com/service/DonationService.java
+++ Server_NGO_GroupProject/src/main/java/com/service/DonationService.java
@@ -1,14 +1,18 @@
 package com.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dto.DonorDTO;
 import com.entity.Donation_Types;
 import com.entity.Donations;
+import com.entity.Donors;
 import com.repository.DonationRepository;
 import com.repository.DonationTypeRepository;
+import com.repository.DonorRepository;
 
 @Service
 public class DonationService {
@@ -17,13 +21,36 @@ public class DonationService {
 	@Autowired
 	DonationTypeRepository dtr;
 	@Autowired
+	DonorRepository donorRepository;
+	@Autowired
+	DonorDTO donorDto;;
+	@Autowired
 	EmailService es;
-	public Donations addDonation(Donations donation) {
-		es.sendEmail(donation.getDonor().getEmail(), 
-				"Recent Donation Confirmation To: "+donation.getDonation_types().getType_name(),
-				"Thank you for your recent contribution to "+donation.getDonation_types().getType_name()+ " of the amount: " +donation.getAmount());
-		dr.save(donation);
-		return donation;
+	
+	public void addDonation(List<Donations> donation) {
+		int storeDonorID=0;
+		int count=0;
+		String email="";
+		String emailTypeName="";
+		int total=0;
+		for(Donations d:donation) {
+			if(count==0) {
+				Donors donor=donorDto.donorModelToEntity(d.getDonor());
+				email=donor.getEmail();
+				donorRepository.save(donor);
+				storeDonorID=donor.getId();
+				count=1;
+			}
+			Donors curDonor=new Donors();
+			curDonor.setId(storeDonorID);
+			d.setDonor(curDonor);
+			emailTypeName+=d.getDonation_types().getType_name()+" / ";
+			total+=d.getAmount();
+			dr.save(d);
+		}
+		es.sendEmail(email, 
+				"Recent Donation Confirmation",
+				"Thank you for your recent contribution to "+emailTypeName+ " of the amount: " +total);	
 	}
 
 	public List<Donations> getAllDonation() {
