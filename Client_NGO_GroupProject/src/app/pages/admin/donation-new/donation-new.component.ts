244d3eb0fad3cc7e0c34a07589c4f5533b4f5101
GoobIsGabe <gabe.harp1996@gmail.com>
2023-11-17 12:39:31 -0500

    add donation type updates in realtime, adjusted css of the user and admin sidebar to be vertically centered

--- Client_NGO_GroupProject/src/app/pages/admin/donation-new/donation-new.component.ts
+++ Client_NGO_GroupProject/src/app/pages/admin/donation-new/donation-new.component.ts
@@ -7,24 +7,32 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-donation-new',
   templateUrl: './donation-new.component.html',
-  styleUrls: ['./donation-new.component.css']
+  styleUrls: ['./donation-new.component.css'],
 })
 export class DonationNewComponent {
-  constructor(public dataService:DonationService, private router:Router, private el: ElementRef){}
-  
+  constructor(
+    public dataService: DonationService,
+    private router: Router,
+    private el: ElementRef
+  ) {}
+
   closeModal() {
-    this.router.navigate(["/admin/add-donation"]);
+    this.router.navigate(['/admin/add-donation']);
   }
 
-  onSubmit(f:NgForm){
-    console.log(f.value)
-   
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+
     //Donation type Json needed because of the response from server
-    this.dataService.addDonationTypeJson(f.value, { responseType: 'text' }).subscribe(data => {
-      console.log(data); // "Donor Type added"
-      this.router.navigate(["/admin/add-donation"]);
-    });
-    
-    
+    this.dataService
+      .addDonationTypeJson(f.value, { responseType: 'text' })
+      .subscribe((data) => {
+        console.log(data); // "Donor Type added"
+        this.router
+          .navigateByUrl('/', { skipLocationChange: true })
+          .then(() => {
+            this.router.navigate(['/admin/add-donation']);
+          });
+      });
   }
 }
