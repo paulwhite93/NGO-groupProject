bd734371b8b60a08780dfca2e5b6c58efce722cc
YYL1222 <99230362+YYL1222@users.noreply.github.com>
2023-11-19 01:57:50 -0600

    Update admin-sidebar.component.ts
    add logout method to clear local storage value

--- Client_NGO_GroupProject/src/app/pages/admin/admin-sidebar/admin-sidebar.component.ts
+++ Client_NGO_GroupProject/src/app/pages/admin/admin-sidebar/admin-sidebar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-sidebar.component.css']
 })
 export class AdminSidebarComponent {
-
+  constructor(public login: AuthenticationService) {}
+  public logout() {
+    this.login.logout();
+    window.location.reload();
+  }
 }
