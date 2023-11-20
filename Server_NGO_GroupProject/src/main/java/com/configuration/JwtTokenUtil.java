5fed24f4b226a0431eb42e1ba08103ef0a9724a1
YYL1222 <99230362+YYL1222@users.noreply.github.com>
2023-11-20 13:37:45 -0600

    put secret key in application.properties

--- Server_NGO_GroupProject/src/main/java/com/configuration/JwtTokenUtil.java
+++ Server_NGO_GroupProject/src/main/java/com/configuration/JwtTokenUtil.java
@@ -27,7 +27,8 @@ import java.util.function.Function;
 public class JwtTokenUtil{
 
 	Logger logger = (Logger) LoggerFactory.getLogger(JwtTokenUtil.class);
-
+	@Value("${jwt.secret-key}")
+    private String SECRET_KEY;
 
     private static String SECRET_KEY="ZXF1aXBtZW50c291dGhmb3J0Y3J5bW9udGhhbnlib2R5YnJpZGdlYWN0aXZlY2FyZWY=";
 
